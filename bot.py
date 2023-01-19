d6e4bf4065db960534c34cf23bf1f9a42d3c4261
Lexpj <lex.janssens@live.nl>
2023-01-19 10:14:50 +0100

    test tree

--- bot.py
+++ bot.py
@@ -1,13 +1,9 @@
 # bot.py
-import os
+
+
 import discord
-from discord.ext import commands
+from discord import app_commands 
 
-import sys
-print("sys.path:\n" + "\n".join(sys.path))
-
-import random
-from APICall import getRhymeWords 
 
 ######### DO NOT CHANGE #########
 import os.path
@@ -16,103 +12,27 @@ with open(os.path.dirname(__file__) + "/../TOKEN.txt","r") as f:
 with open(os.path.dirname(__file__) + "/../branch.txt","r") as f:
     BRANCH = f.readline().rstrip()
 #################################
-    
-client = commands.Bot(intents=discord.Intents.all(),command_prefix="!")
 
 
-############# GIT ###############
-import subprocess
-@client.command()
-async def git(ctx, *args):
-    if len(args) == 0:
-        embedVar = discord.Embed(title="Git", description="Possible commands", color=0xff0000)
-        embedVar.add_field(name="!git status", value="Current branch the bot is in", inline=False)
-        embedVar.add_field(name="!git checkout [branch]", value="Checkout a different branch. RELOAD ON EXECUTION", inline=False)
-        await ctx.send(embed=embedVar)  
-    elif args[0] == "status":
-        message = f"Currently on branch '{BRANCH}'"
-        with open(os.path.dirname(__file__) + "/../branch.txt","r") as f:
-            newbranch = f.readline().rstrip()
-        if newbranch != BRANCH:
-            message += f"\nAfter reboot on branch '{newbranch}'"
-        await ctx.send(message)
-        
-    elif args[0] == "checkout":
-        with open(os.path.dirname(__file__) + "/../branch.txt","w") as f:
-            f.write(args[1])
-        await ctx.send(f"After reboot, starting up on branch '{args[1]}'")
-#################################
+class client(discord.Client):
+    def __init__(self):
+        super().__init__()
+        self.synced = False #we use this so the bot doesn't sync commands more than once
 
-####### DEPENDENCIES ############
-@client.command()
-async def pip(ctx, *args):
-    if len(args) == 0:
-        embedVar = discord.Embed(title="Pip", description="Possible commands", color=0x0000ff)
-        embedVar.add_field(name="!pip install", value="Installs a package", inline=False)
-        embedVar.add_field(name="!pip uninstall", value="Uninstalls a package", inline=False)
-        await ctx.send(embed=embedVar)
-    elif args[0] == "install":
-        output = subprocess.check_output(f"sudo pip install {args[1]}", shell=True)
-        await ctx.send(output.decode("utf-8") ) 
-    elif args[0] == "uninstall":
-        output = subprocess.check_output(f"sudo pip uninstall {args[1]}", shell=True)
-        await ctx.send(output.decode("utf-8") ) 
-#################################
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced: #check if slash commands have been synced 
+            await tree.sync() #guild specific: leave blank if global (global registration can take 1-24 hours)
+            self.synced = True
+        await self.change_presence(activity=discord.Game(name=f"Active>{BRANCH}"))
+        print(f"We have logged in as {self.user}.")
 
-@client.command()
-async def reload(ctx, *args):
-    await ctx.channel.send("Rebooting...")
-    await client.change_presence(activity=discord.Game(name="Rebooting..."))
-    os.system("sudo reboot")
+aclient = client()
+tree = app_commands.CommandTree(aclient)
 
-@client.command()
-async def rhyme(ctx, *args):
-    
-    ## HELP
-    if len(args) == 0:
-        embedVar = discord.Embed(title="Rhyme bot", description="Possible commands", color=0x00ff00)
-        embedVar.add_field(name="!rhyme [word]", value="Receive a random rhyme word", inline=False)
-        embedVar.add_field(name="!rhyme [word] -all", value="Receive all rhyme words", inline=False)
-        embedVar.add_field(name="!rhyme [word] -best [x=1]", value="Receive the best x rhyme words. By default: 1", inline=False)
-        embedVar.add_field(name="!rhyme [word] -worst [x=1]", value="Receive the worst x rhyme words. By default: 1", inline=False)
-        await ctx.send(embed=embedVar)  
-    
-    else:
-        rhymeWords = getRhymeWords(args[0])
-    
-        # No rhyme words found:
-        if len(rhymeWords) == 0:
-            await ctx.channel.send(f"No words found that rhyme with '{args[0]}'")
+@tree.command(name = 'tester', description='testing') #guild specific slash command
+async def slash2(interaction: discord.Interaction):
+    await interaction.response.send_message(f"I am working! I was made with Discord.py!", ephemeral = True) 
 
-        elif "-all" in args[1:]:
-            await ctx.channel.send(', '.join(rhymeWords))
+aclient.run(TOKEN)
 
-        elif "-best" in args[1:]:
-            if len(args) > 2:
-                await ctx.channel.send(', '.join(rhymeWords[:int(args[2])]))
-            else:
-                await ctx.channel.send(rhymeWords[0])
-        elif "-worst" in args[1:]:
-            if len(args) > 2:
-                await ctx.channel.send(', '.join(rhymeWords[-int(args[2]):]))
-            else:
-                await ctx.channel.send(rhymeWords[-1])
-        else:
-            await ctx.channel.send(random.choice(rhymeWords))
-
-@client.event
-async def on_ready():
-    await client.change_presence(activity=discord.Game(name=f"Active>{BRANCH}"))
-    print(f'{client.user} has connected to Discord!')
-
-@client.event
-async def on_message(message):
-    
-    if message.author == client.user:
-        return
-    
-
-    await client.process_commands(message)
-                
-            
-client.run(TOKEN)
