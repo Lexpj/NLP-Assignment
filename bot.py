78a0f55496149f89a530ee4aefc8fa6e437bfec8
Lexpj <lex.janssens@live.nl>
2023-01-19 14:43:20 +0100

    added rhyme and pip

--- bot.py
+++ bot.py
@@ -61,17 +61,11 @@ async def cmd(ctx: interactions.CommandContext, sub_command: str, second_option:
         
 
 ############# GIT ###############
-import subprocess
 @bot.command(
     name="git",
     description="Use git commands to switch branches and check current branch",
     scope=GUILD,
     options=[
-        interactions.Option(
-            name="help",
-            description="Overview of all commands",
-            type=interactions.OptionType.SUB_COMMAND,
-        ),
         interactions.Option(
             name="status",
             description="Check current branch of the bot",
@@ -93,13 +87,7 @@ import subprocess
     ],
 )
 async def git(ctx: interactions.CommandContext, sub_command: str, branch: str = ""):
-    if sub_command == "help":
-        embedVar = discord.Embed(title="Git", description="Possible commands", color=0xff0000)
-        embedVar.add_field(name="!git status", value="Current branch the bot is in", inline=False)
-        embedVar.add_field(name="!git checkout [branch]", value="Checkout a different branch. RELOAD ON EXECUTION", inline=False)
-        await ctx.send(embed=embedVar)  
-    
-    elif sub_command == "status":
+    if sub_command == "status":
         message = f"Currently on branch '{BRANCH}'"
         with open(os.path.dirname(__file__) + "/../branch.txt","r") as f:
             newbranch = f.readline().rstrip()
@@ -113,7 +101,115 @@ async def git(ctx: interactions.CommandContext, sub_command: str, branch: str = 
         await ctx.send(f"After reboot, starting up on branch '{branch}'")
 #################################
 
-      
+############# PIP ###############
+@bot.command(
+    name="pip",
+    description="Use pip commands to install or uninstall packages",
+    scope=GUILD,
+    options=[
+        interactions.Option(
+            name="install",
+            description="Install a package",
+            type=interactions.OptionType.SUB_COMMAND,
+            options=[
+                interactions.Option(
+                    name="package",
+                    description="Package to be installed",
+                    type=interactions.OptionType.STRING,
+                    required=True,
+                ),
+            ],
+        ),
+        interactions.Option(
+            name="uninstall",
+            description="Uninstall a package",
+            type=interactions.OptionType.SUB_COMMAND,
+            options=[
+                interactions.Option(
+                    name="package",
+                    description="Package to be uninstalled",
+                    type=interactions.OptionType.STRING,
+                    required=True,
+                ),
+            ],
+        ),
+    ],
+)
+async def pip(ctx: interactions.CommandContext, sub_command: str, package: str = ""):
+    if sub_command == "install":
+        os.system(f"sudo pip install {package}")
+        await ctx.send(f"{package} installed!") 
+    elif sub_command == "uninstall":
+        os.system(f"sudo pip uninstall {package}")
+        await ctx.send(f"{package} installed!")  
+#################################  
+
+############ RHYME ##############
+@bot.command(
+    name="rhyme",
+    description="Use rhyme commands",
+    scope=GUILD,
+    options=[
+        interactions.Option(
+            name="prompt",
+            description="Sentence/phrase/word to rhyme",
+            type=interactions.OptionType.STRING,
+            required=True,
+            options=[
+                interactions.Option(
+                    name="best",
+                    description="Get the best options for rhyme words",
+                    type=interactions.OptionType.SUB_COMMAND,
+                    required=False,
+                    options=[
+                        interactions.Option(
+                            name="items",
+                            description="Number of items",
+                            type=interactions.OptionType.INTEGER,
+                            required=True,
+                        ),
+                    ]
+                ),
+                interactions.Option(
+                    name="worst",
+                    description="Get the worst options for rhyme words",
+                    type=interactions.OptionType.SUB_COMMAND,
+                    required=False,
+                    options=[
+                        interactions.Option(
+                            name="items",
+                            description="Number of items",
+                            type=interactions.OptionType.INTEGER,
+                            required=True,
+                        ),
+                    ]
+                ),
+                interactions.Option(
+                    name="all",
+                    description="Get all options for rhyme words",
+                    type=interactions.OptionType.SUB_COMMAND,
+                    required=False,
+                ),
+            ],
+        ),
+    ],
+)
+
+async def rhyme(ctx: interactions.CommandContext, sub_command: str = None, prompt: str = "", items: int = 1):
+    rhymeWords = getRhymeWords(prompt.split()[-1])
+    
+    if len(rhymeWords) == 0:
+        await ctx.send(f"No words found that rhyme with '{prompt.split()[-1]}'")
+    
+    if sub_command == None:
+        await ctx.send(random.choice(rhymeWords))
+    elif sub_command == "all":
+        await ctx.send(', '.join(rhymeWords))
+    elif sub_command == "best":
+        await ctx.send(', '.join(rhymeWords[:items]))
+    elif sub_command == "worst":
+        await ctx.send(', '.join(rhymeWords[-items:]))
+            
       
 @bot.command(
     name="reboot",
@@ -131,7 +227,7 @@ async def on_ready():
     global _ready
     if not _ready:
         #await bot.change_presence(presence=interactions.ClientPresence(f"Active on '{BRANCH}'"))
-        print(f'{bot.user} has connected to Discord!')  
+        print(f'Bot has connected to Discord!')  
         _ready = True
           
 bot.start()
