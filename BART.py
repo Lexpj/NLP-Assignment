6045762e57a951e3b036a0e8bccb462637eb3d52
Lexpj <69421101+Lexpj@users.noreply.github.com>
2023-01-21 23:03:36 +0100

    Update BART.py

--- BART.py
+++ BART.py
@@ -1,11 +1,16 @@
 from transformers import BartForConditionalGeneration, BartTokenizer
 
-def BART(sentence, rhymeword):
-    model = BartForConditionalGeneration.from_pretrained("facebook/bart-large", forced_bos_token_id=0)
-    tok = BartTokenizer.from_pretrained("facebook/bart-large")
+class BART:
+    
+    def __init__(self):
+        self.model = BartForConditionalGeneration.from_pretrained("facebook/bart-large", forced_bos_token_id=0)
+        self.tok = BartTokenizer.from_pretrained("facebook/bart-large")
 
-    example_english_phrase = sentence + " <mask> " + rhymeword
-    batch = tok(example_english_phrase, return_tensors="pt")
-    generated_ids = model.generate(batch["input_ids"])
-
-    return(tok.batch_decode(generated_ids, skip_special_tokens=True)[0])
+    def gen(self,sentence, rhymeword):
+        
+        example_english_phrase = sentence + " <mask> " + rhymeword
+        batch = self.tok(example_english_phrase, return_tensors="pt")
+        generated_ids = self.model.generate(batch["input_ids"])
+        res = self.tok.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        print(res)
+        return(res)
